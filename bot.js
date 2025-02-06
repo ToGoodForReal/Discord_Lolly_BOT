f6867c19f04cfb05d592648e75620aca10a24b65
Elytus <elitinho2502@hotmail.com>
2025-02-06 09:47:03 -0400

    Update index to bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,20 @@
 const discord = require('discord.js');
-const client = new discord.Client();
 const dotenv = require('dotenv');
 dotenv.config();
 
-client.on('ready', () => {
-  console.log('Bot is ready');
+
+const client = new discord.Client({
+  intents: Object.values(discord.GatewayIntentBits),
+  Partials: [
+      discord.Partials.message,
+      discord.Partials.channel,
+      discord.Partials.Reaction
+  ],
 });
+client.login(process.env.TOKEN);
 
 client.on('message', message => {
   if (message.content.startWith('!ping')) {
     message.channel.send('Para com essa Piada ai pô');
   }
 });
-client.login(process.env.TOKEN);
